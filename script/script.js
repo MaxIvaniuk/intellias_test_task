e5a17ab755574705f90e32212aa079c74d2bb869
MaxIvaniuk <meteorit614@gmail.com>
2021-11-20 00:22:54 +0200

    add skills and education section

--- script/script.js
+++ script/script.js
@@ -1,7 +1,6 @@
 'use strict';
 
 // Mobile navigation
-
 let mobileNav = document.querySelector('.mobile_nav'),
     toggler = document.querySelector('.mobile_toggle__input');
 
@@ -15,8 +14,15 @@ let header = document.querySelector('.header');
 window.addEventListener('scroll', () => {
     if (window.scrollY > 200) {
         header.classList.add('sticky')
-        console.log('scroll is more then 100')
     } else {
         header.classList.remove('sticky')
     }
 })
+
+// Accordeon
+// let accordeon = document.querySelector('.hard_skills'),
+//     opener = document.querySelector('.hard_skills__list');
+
+// accordeon.addEventListener('click', () => {
+//     opener.classList.add('active');
+// })
